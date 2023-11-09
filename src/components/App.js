08c3e6a31593feaa42c16704d827c8b50a4c5d97
Carlos Gomez <cgomezlugo@hotmail.com>
2023-11-09 14:49:58 -0400

    features/declarative_paradig: set popups opening

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,36 @@ import Footer from './Footer';
 
 function App() {
   const handleEditAvatarClick = () => {
-    document.querySelector('#popup').classList.add('active');
+    document.querySelector('.popup_type_avatar-form').classList.add('active');
     document.querySelector('#avatar-form').classList.add('active');
   };
 
   const handleEditProfileClick = () => {
-    document.querySelector('#popup').classList.add('active');
+    document.querySelector('.popup_type_profile-form ').classList.add('active');
     document.querySelector('#profile-form').classList.add('active');
 
   };
 
   const handleAddPlaceClick = () => {
-    document.querySelector('#popup').classList.add('active');
+    document.querySelector('.popup_type_add-form ').classList.add('active');
     document.querySelector('#add-form').classList.add('active');
   };
+
+  const closeAllPopups = () => {
+    document.querySelector(".popup").classList.remove("active");
+    document.querySelector(".form").classList.remove("active");
+  };
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      closeAllPopups();
+    }
+  });
+  document.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("popup")) {
+      closeAllPopups();
+    }
+  });
+
 
   return (
     <div>
@@ -26,6 +42,7 @@ function App() {
       onEditProfileClick = {handleEditProfileClick}
       onAddPlaceClick = {handleAddPlaceClick}
       onEditAvatarClick = {handleEditAvatarClick}
+      onClose = {closeAllPopups}
       />
       <Footer />
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import "../index.css";
 import PopupWithForm from "./PopupWithForm";
 import pencil from "../images/Vectoredit-pencil2.svg";
 
-function Main({onEditAvatarClick, onEditProfileClick, onAddPlaceClick}) {
+function Main({ onEditAvatarClick, onEditProfileClick, onAddPlaceClick, onClose }) {
   return (
     <div>
       <div className="page">
@@ -38,104 +38,115 @@ function Main({onEditAvatarClick, onEditProfileClick, onAddPlaceClick}) {
           </div>
           <section className="cards"></section>
 
-          <section id="popup" className="popup">
-            <PopupWithForm title="Cambiar Foto de Perfil" name="avatar-form">
-              <input
-                type="url"
-                id="url-input-avatar"
-                className="form__input"
-                placeholder="Introduce URL (https://... o http://...)"
-                required
-              />
-              <span className="url-input-avatar-error form__error"></span>
-              <div className="form__button-container">
-                <button
-                  id="button-avatar-save"
-                  type="submit"
-                  className="form__button"
-                >
-                  Guardar
-                </button>
-                <button
-                  id="button-avatar-saving"
-                  className="form__button-saving"
-                >
-                  Guardando...
-                </button>
-              </div>
-            </PopupWithForm>
-            <PopupWithForm title="Editar perfil" name="profile-form">
-              <input
-                id="text-input-name"
-                className="form__input"
-                type="text"
-                placeholder="Nombre"
-                minLength="2"
-                maxLength="40"
-                required
-              />
-              <span className="text-input-name-error form__error"></span>
-              <input
-                id="text-input-about"
-                type="text"
-                className="form__input"
-                placeholder="Acerca de mi"
-                minLength="2"
-                maxLength="200"
-                required
-              />
-              <span className="text-input-about-error form__error"></span>
-              <div className="form__button-container">
-                <button
-                  id="button-user-save"
-                  type="submit"
-                  className="form__button"
-                >
-                  Guardar
-                </button>
-                <button id="button-user-saving" className="form__button-saving">
-                  Guardando...
-                </button>
-              </div>
-            </PopupWithForm>
-            <PopupWithForm title="Nuevo Lugar" name="add-form">
-              <input
-                id="text-input-place"
-                type="text"
-                className="form__input"
-                placeholder="Nombre del lugar"
-                minLength="2"
-                maxLength="30"
-                required
-              />
-              <span className="text-input-place-error form__error"></span>
-              <input
-                type="url"
-                id="url-input-image"
-                className="form__input"
-                placeholder="Introduce URL (https://... o http://...)"
-                required
-              />
-              <span className="url-input-image-error form__error"></span>
-              <div className="form__button-container">
-                <button
-                  id="button-add-save"
-                  type="submit"
-                  className="form__button"
-                >
-                  Crear
-                </button>
-                <button id="button-add-saving" className="form__button-saving">
-                  Guardando...
-                </button>
-              </div>
-            </PopupWithForm>
-            <PopupWithForm title="¿Estás seguro/a?" name="sure-form">
-              <button type="button" className="form__button">
-                Sí
+          <PopupWithForm title="Cambiar Foto de Perfil" name="avatar-form">
+            <button
+              type="button"
+              className="form__close"
+              onClick={onClose}
+            ></button>
+            <input
+              type="url"
+              id="url-input-avatar"
+              className="form__input"
+              placeholder="Introduce URL (https://... o http://...)"
+              required
+            />
+            <span className="url-input-avatar-error form__error"></span>
+            <div className="form__button-container">
+              <button
+                id="button-avatar-save"
+                type="submit"
+                className="form__button"
+              >
+                Guardar
               </button>
-            </PopupWithForm>
-            {/*
+              <button id="button-avatar-saving" className="form__button-saving">
+                Guardando...
+              </button>
+            </div>
+          </PopupWithForm>
+          <PopupWithForm title="Editar perfil" name="profile-form">
+          <button
+              type="button"
+              className="form__close"
+              onClick={onClose}
+            ></button>
+            <input
+              id="text-input-name"
+              className="form__input"
+              type="text"
+              placeholder="Nombre"
+              minLength="2"
+              maxLength="40"
+              required
+            />
+            <span className="text-input-name-error form__error"></span>
+            <input
+              id="text-input-about"
+              type="text"
+              className="form__input"
+              placeholder="Acerca de mi"
+              minLength="2"
+              maxLength="200"
+              required
+            />
+            <span className="text-input-about-error form__error"></span>
+            <div className="form__button-container">
+              <button
+                id="button-user-save"
+                type="submit"
+                className="form__button"
+              >
+                Guardar
+              </button>
+              <button id="button-user-saving" className="form__button-saving">
+                Guardando...
+              </button>
+            </div>
+          </PopupWithForm>
+          <PopupWithForm title="Nuevo Lugar" name="add-form">
+          <button
+              type="button"
+              className="form__close"
+              onClick={onClose}
+            ></button>
+            <input
+              id="text-input-place"
+              type="text"
+              className="form__input"
+              placeholder="Nombre del lugar"
+              minLength="2"
+              maxLength="30"
+              required
+            />
+            <span className="text-input-place-error form__error"></span>
+            <input
+              type="url"
+              id="url-input-image"
+              className="form__input"
+              placeholder="Introduce URL (https://... o http://...)"
+              required
+            />
+            <span className="url-input-image-error form__error"></span>
+            <div className="form__button-container">
+              <button
+                id="button-add-save"
+                type="submit"
+                className="form__button"
+              >
+                Crear
+              </button>
+              <button id="button-add-saving" className="form__button-saving">
+                Guardando...
+              </button>
+            </div>
+          </PopupWithForm>
+          <PopupWithForm title="¿Estás seguro/a?" name="sure-form">
+            <button type="button" className="form__button">
+              Sí
+            </button>
+          </PopupWithForm>
+          {/*
             <form id="sure-form" className="form">
               <h2 className="form__title">¿Estás seguro/a?</h2>
               <button
@@ -148,16 +159,15 @@ function Main({onEditAvatarClick, onEditProfileClick, onAddPlaceClick}) {
               </button>
             </form> */}
 
-            <div className="image-container">
-              <button type="button" className="form__close"></button>
-              <img
-                alt="imagen del gran canon"
-                src="https://images.unsplash.com/photo-1643252494989-81cd0b5bead2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
-                className="image-container__photo"
-              />
-              <p className="image-container__name"></p>
-            </div>
-          </section>
+          <div className="image-container">
+            <button type="button" className="form__close"></button>
+            <img
+              alt="imagen del gran canon"
+              src="https://images.unsplash.com/photo-1643252494989-81cd0b5bead2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+              className="image-container__photo"
+            />
+            <p className="image-container__name"></p>
+          </div>
         </main>
       </div>
     </div>
