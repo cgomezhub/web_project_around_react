673cc3928fa0f3ee8e4cb307bcf147a4235c8c48
Carlos Gomez <cgomezlugo@hotmail.com>
2023-11-12 12:53:30 -0400

    features/Popup_image: set styles

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,40 @@
-import Api from "../components/Api";
-
-const ap = new Api({
+class Api {
+    constructor(options) {
+      this.address = options.address;
+      this.headers = options.headers;
+    }
+    getUserData() {
+      return fetch(`${this.address}/users/me`, {
+        headers: this.headers,
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject("Error: " + res.status);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    getCardData() {
+      return fetch(`${this.address}/cards`, {
+        headers: this.headers,
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject('Error: ' + res.status);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  
+    
+}
+const api = new Api({
   address: 'https://around.nomoreparties.co/v1/web_es_09',
   headers: {
     authorization: '24db7356-9f7a-470a-979e-9ec3f25f6f02',
@@ -8,4 +42,4 @@ const ap = new Api({
   },
 });
 
-export default ap;
+export default api;
