0fece68e24f066feb1cdb8df3792d33e0f283db3
Carlos Gomez <cgomezlugo@hotmail.com>
2023-11-13 14:52:41 -0400

    main: fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-class Api {
+export default class Api {
     constructor(options) {
       this.address = options.address;
       this.headers = options.headers;
@@ -34,12 +34,5 @@ class Api {
   
     
 }
-const api = new Api({
-  address: 'https://around.nomoreparties.co/v1/web_es_09',
-  headers: {
-    authorization: '24db7356-9f7a-470a-979e-9ec3f25f6f02',
-    "Content-Type": "application/json",
-  },
-});
 
-export  {api};
+
